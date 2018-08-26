616598c21ed4ddd68b4e9bb7cd95ea3c91e5d5a6
divini <jeffrdaquioag@gmail.com>
2018-08-26 15:06:35 -0700

    First draft

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,5 +1,5 @@
 // Word/Hint pairing arrays
-const word = ["MOMONGA", "NAZARICK", "YGGDRASIL", "DEMIURGE", "PlEADIES"]
+const word = ["MOMONGA", "NAZARICK", "YGGDRASIL", "DEMIURGE", "PLEADIES"]
 const hint = [  "This is Ainz Oal Gown's actual name.", 
                 "This is the home where Ainz and his guardians live.",
                 "This is the name of the game Overlord's world is based off of.",
@@ -11,48 +11,44 @@ var wordElement = document.getElementById('word');
 var hintElement = document.getElementById('hint');
 var winsElement = document.getElementById('wins');
 var loseElement = document.getElementById('lose');
+var guessElement = document.getElementById('guesses');
 var lettersGuessedElement = document.getElementById('lettersGuessed');
+var sysElement = document.getElementById('sysMessage');
 
 class Hangman{
 
-    constructor(cw = word[0], h = hint[0], w = 0, l = 0) {        
+    constructor(cw = "Overlord", h = "This page's theme!", w = 0, l = 0) {        
         this.correctWord = cw;
         this.currentHint = h;
         this.wins = w;
         this.lose = l;
-        this.guesses = 13;      // Number of guesses allowed per attempt - change as needed
+        this.guesses = 14;       // Number of guesses allowed per attempt
         this.letters = [];      // Array of letters guessed
         this.currentWord = [];  // The displayed word being guessed  
     }
 
-    setWins(wins) {
-        this.wins = wins;
-    }
+    // Starts/Restarts the hangman game
+    initializeGame() {
+        // Set random word/hint pairing
+        var index = Math.floor(Math.random() * word.length);
+        this.correctWord = word[index];
+        this.currentHint = hint[index];
+        this.guesses = this.calcGuesses(this.correctWord.length);
+        this.letters = [];
 
-    setLose(lose) {
-        this.lose = lose;
-    }
-
-    getWins() {
-        return this.wins;
-    }
-
-    getLose() {
-        return this.lose;
-    }
-
-    getLetters() {
-        return this.letters;
-    }
-
-    // Initializes the current word (that needs to be guessed) in underscores
-    initializeWord() {
+        // Fill in the word display with underscores
         this.currentWord = [];
         for (var i = 0; i < this.correctWord.length; i++) {
             this.currentWord.push('_');
         }
+
+        // Display it all in html
         wordElement.innerHTML = this.currentWord.join(' ');
         hintElement.innerHTML = this.currentHint;
+        winsElement.innerHTML = (" " + this.wins);
+        loseElement.innerHTML = (" " + this.lose);
+        guessElement.innerHTML = (" " + this.guesses);
+        lettersGuessedElement.innerHTML = "";
     }
 
     // Takes the letter guessed by the user and test it vs the correct word and letters already guessed
@@ -77,6 +73,8 @@ class Hangman{
                 this.letters.push(letter);      // Adds letter guessed into end of array
                 lettersGuessedElement.innerHTML = this.letters.join(', ');  // Adds leading comma
                 this.guesses--;
+                guessElement.innerHTML = this.guesses;
+
                 if (!this.isWin())   // Check win condition, check lose condition if win condition is false
                     this.isLose();
             }
@@ -89,34 +87,43 @@ class Hangman{
         }
     }
 
-    isWin(){    // Check win condition
-        if (currentWord.indexOf('_') === -1) {
+    // Helper function containing formula to calculate guesses allowed - can change if needed
+    calcGuesses(wordLength) {
+        return Math.floor(wordLength * 1.3);
+    }
+
+    // Helper function that checks win condition
+    isWin(){    
+        if (this.currentWord.indexOf('_') == -1) {      // Works for words with no spaces.  Must change if it has spaces
             // You win message
+            //wordElement.innerHTML = this.correctWord;
+            //try settimeout here....
+            alert("You win!");
             this.wins++;
+            this.initializeGame();
             return true;
         }
     }
 
-    isLose(){   // Check lose condition
-        if (this.guesses);
+    // Helper function that checks lose condition
+    isLose(){   
+        if (this.guesses == 0) {
+            // Lose message
+            alert("You lose");
+            this.lose++;
+            this.initializeGame();
+        }
     }
 
 }
 
 //main                                                      
-var index = Math.floor(Math.random() * word.length);       // Set Random starting point
-var hangman = new Hangman(word[index], hint[index]);
-
-hangman.initializeWord();
-winsElement.textContent += (" " + hangman.wins);
-loseElement.textContent += (" " + hangman.lose);
+var hangman = new Hangman();
+hangman.initializeGame();
 
 document.onkeyup = function(event) {
     hangman.guessLetter(event.key);
 }
-
-
-// alert(hangman.currentWord + " - " + hangman.currentHint);
 
 
 
